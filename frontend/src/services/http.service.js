b3c9d9590093827ebeef033af414ecd7a58611c0
Emanuel Ben Hamo <embenh@gmail.com>
2020-04-06 22:28:43 +0300

    updated build script in package json
    build result is the 'dist' directory.
    now it will be created under the backend/public directory

--- frontend/src/services/http.service.js
+++ frontend/src/services/http.service.js
@@ -2,7 +2,7 @@ import router from '../router/index.js';
 
 const BASE_URL = process.env.NODE_ENV === 'production' ?
     '/api/' :
-    '//localhost:3030/api/'
+    '//localhost:3030/api/';
 
 import Axios from 'axios';
 var axios = Axios.create({
